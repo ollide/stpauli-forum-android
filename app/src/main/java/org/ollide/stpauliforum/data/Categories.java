8d6f2014eca1cbc50266696af34ab7f36e7aa823
Oliver Degener <o.degener@monday-consulting.com>
2016-03-09 14:20:08 +0100

    full list of Categories and Forums

--- app/src/main/java/org/ollide/stpauliforum/data/Categories.java
+++ app/src/main/java/org/ollide/stpauliforum/data/Categories.java
@@ -9,9 +9,9 @@ public final class Categories {
 
     public static final Category RUND_UM_DEN_SPORT = new Category(1, "Rund um den Sport", Forums.CAT_SPORT);
     public static final Category RUND_UM_DIE_FANSZENE = new Category(15, "Rund um die Fanszene", Forums.CAT_FANSZENE);
-    public static final Category OFFIZIELLES = new Category(16, "Offizielles", Forums.CAT_SPORT);
-    public static final Category ENGLISH_FORUM = new Category(2, "English Forum", Forums.CAT_SPORT);
-    public static final Category SONSTIGES = new Category(3, "Sonstiges", Forums.CAT_SPORT);
+    public static final Category OFFIZIELLES = new Category(16, "Offizielles", Forums.CAT_OFFIZIELLES);
+    public static final Category ENGLISH_FORUM = new Category(2, "English Forum", Forums.CAT_ENGLISH);
+    public static final Category SONSTIGES = new Category(3, "Sonstiges", Forums.CAT_SONSTIGES);
 
     public static final List<Category> ALL = new ArrayList<>(5);
     static {
--- app/src/main/java/org/ollide/stpauliforum/data/Forums.java
+++ app/src/main/java/org/ollide/stpauliforum/data/Forums.java
@@ -22,6 +22,20 @@ public final class Forums {
     public static final Forum FANS_WELTWEIT = new Forum(43, "Fans weltweit", "", Collections.<Topic>emptyList());
     public static final Forum FLOHMARKT = new Forum(13, "Flohmarkt", "", Collections.<Topic>emptyList());
 
+    public static final Forum FCSTPAULI = new Forum(3, "FC St. Pauli", "Vorstand, Abteilungen, Vermarktung", Collections.<Topic>emptyList());
+    public static final Forum FANSHOP = new Forum(44, "Fanshop/Merchandising", "Klamotten, Merch und sonstiger Krams direkt aus dem Fanshop", Collections.<Topic>emptyList());
+    public static final Forum KARTENCENTER = new Forum(78, "Kartencenter", "Für alle Fragen und Diskussionen rund um die Kartensituation und das Ticketing, aber nicht für Karten-Verkäufe/ -Gesuche (die bitte im Flohmarkt in den jeweiligen Spieltags-Sammelthread)", Collections.<Topic>emptyList());
+    public static final Forum VIVA_CON_AGUA = new Forum(40, "Viva con Agua de Sankt Pauli", "Alles rund um das Projekt \"Sauberes Trinkwasser\" von Benny Adrion & Co.", Collections.<Topic>emptyList());
+
+    public static final Forum ABROAD = new Forum(5, "For our visitors from abroad", "Only english postings in this forum please.", Collections.<Topic>emptyList());
+
+    public static final Forum FAQ = new Forum(7, "FAQ / Anregungen / Kritik / News", "Probleme, Fragen oder einfach nur Feedback? Moderatoren- oder Userkritik? Kein Problem, hier bist du richtig!", Collections.<Topic>emptyList());
+    public static final Forum LABERECKE = new Forum(6, "Klönschnack / Laberecke", "Hier könnt ihr euren Klönschnack durchführen, Spielberichte und Fotos ankündigen usw...", Collections.<Topic>emptyList());
+    public static final Forum UNPAULITISCH = new Forum(11, "Unpaulitisch?", "Tages- und Weltgeschehen / Politik, die nicht in die anderen Kategorien passt.", Collections.<Topic>emptyList());
+    public static final Forum RATGEBER = new Forum(75, "Ratgeber und Lebenshilfe", "Empfehlungen, Tipps und Tricks, Informationsaustausch, Ratgeber...", Collections.<Topic>emptyList());
+    public static final Forum INTERNETHILFE = new Forum(8, "Internethilfe / Computerfragen / Webmasteraustausch", "Der Platz für technische Fragen, die nicht direkt mit diesem Forum zu tun haben. // Fragen zum Forum bitte in FAQ.", Collections.<Topic>emptyList());
+    public static final Forum TESTFORUM = new Forum(10, "Testforum", "Für Newbies, testet alles aus ;-)", Collections.<Topic>emptyList());
+
     public static final List<Forum> CAT_SPORT = new ArrayList<>(4);
     static {
         CAT_SPORT.add(FCSP_FUSSBALLGOETTER);
@@ -39,6 +53,32 @@ public final class Forums {
         CAT_FANSZENE.add(SOZIALROMANTIKER);
         CAT_FANSZENE.add(FANS_WELTWEIT);
         CAT_FANSZENE.add(FLOHMARKT);
+    }
+
+    public static final List<Forum> CAT_OFFIZIELLES = new ArrayList<>(4);
+
+    static {
+        CAT_OFFIZIELLES.add(FCSTPAULI);
+        CAT_OFFIZIELLES.add(FANSHOP);
+        CAT_OFFIZIELLES.add(KARTENCENTER);
+        CAT_OFFIZIELLES.add(VIVA_CON_AGUA);
+    }
+
+    public static final List<Forum> CAT_ENGLISH = new ArrayList<>(1);
+
+    static {
+        CAT_ENGLISH.add(ABROAD);
+    }
+
+    public static final List<Forum> CAT_SONSTIGES = new ArrayList<>(6);
+
+    static {
+        CAT_SONSTIGES.add(FAQ);
+        CAT_SONSTIGES.add(LABERECKE);
+        CAT_SONSTIGES.add(UNPAULITISCH);
+        CAT_SONSTIGES.add(RATGEBER);
+        CAT_SONSTIGES.add(INTERNETHILFE);
+        CAT_SONSTIGES.add(TESTFORUM);
     }
 
     private Forums() {

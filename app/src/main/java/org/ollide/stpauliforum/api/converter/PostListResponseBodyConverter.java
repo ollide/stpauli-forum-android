12f311b42531b9cfba8061da2d10e0f7fe9cce39
Oliver Degener <o.degener@monday-consulting.com>
2016-09-01 18:57:33 +0200

    PostList parser: added 15 new emojis

--- app/src/main/java/org/ollide/stpauliforum/api/converter/PostListResponseBodyConverter.java
+++ app/src/main/java/org/ollide/stpauliforum/api/converter/PostListResponseBodyConverter.java
@@ -62,6 +62,36 @@ public class PostListResponseBodyConverter extends HtmlConverter<PostList> {
     public static final int UNICODE_EEK = 0x1F631;
     public static final String EMOJI_COOL = "<img src=\"images/smiles/cool.gif\" alt=\"cool.gif\" border=\"0\">";
     public static final int UNICODE_COOL = 0x1F60E;
+    public static final String EMOJI_TONGUE = "<img src=\"images/smiles/tongue.gif\" alt=\"tongue.gif\" border=\"0\">";
+    public static final int UNICODE_TONGUE = 0x1F61B;
+    public static final String EMOJI_HAPPY = "<img src=\"images/smiles/happy.gif\" alt=\"happy.gif\" border=\"0\">";
+    public static final int UNICODE_HAPPY = 0x1F606;
+    public static final String EMOJI_NIXWEISS = "<img src=\"images/smiles/nixweiss.gif\" alt=\"nixweiss.gif\" border=\"0\">";
+    public static final int UNICODE_NIXWEISS = 0x1F914;
+    public static final String EMOJI_DAGEGEN = "<img src=\"images/smiles/bindagegen.gif\" alt=\"bindagegen.gif\" border=\"0\">";
+    public static final int UNICODE_DAGEGEN = 0x1F645;
+    public static final String EMOJI_VIELPOSTEN = "<img src=\"images/smiles/vielposten.gif\" alt=\"vielposten.gif\" border=\"0\">";
+    public static final int UNICODE_VIELPOSTEN = 0x270F;
+    public static final String EMOJI_SMOKE = "<img src=\"images/smiles/icon_smoke.gif\" alt=\"icon_smoke.gif\" border=\"0\">";
+    public static final int UNICODE_SMOKE = 0x1F60E;
+    public static final String EMOJI_NAJA = "<img src=\"images/smiles/naja.gif\" alt=\"naja.gif\" border=\"0\">";
+    public static final int UNICODE_NAJA = 0x1F615;
+    public static final String EMOJI_ROLLEYES = "<img src=\"images/smiles/icon_rolleyes.gif\" alt=\"icon_rolleyes.gif\" border=\"0\">";
+    public static final int UNICODE_ROLLEYES = 0x1F644;
+    public static final String EMOJI_RAZZ = "<img src=\"images/smiles/icon_razz.gif\" alt=\"icon_razz.gif\" border=\"0\">";
+    public static final int UNICODE_RAZZ = 0x1F61B;
+    public static final String EMOJI_LACH = "<img src=\"images/smiles/lach.gif\" alt=\"lach.gif\" border=\"0\">";
+    public static final int UNICODE_LACH = 0x1F604;
+    public static final String EMOJI_KOTZ = "<img src=\"images/smiles/kotz.gif\" alt=\"kotz.gif\" border=\"0\">";
+    public static final int UNICODE_KOTZ = 0x1F922;
+    public static final String EMOJI_LACHLAUT = "<img src=\"images/smiles/lachlaut.gif\" alt=\"lachlaut.gif\" border=\"0\">";
+    public static final int UNICODE_LACHLAUT = 0x1F603;
+    public static final String EMOJI_WALL = "<img src=\"images/smiles/wall.gif\" alt=\"wall.gif\" border=\"0\">";
+    public static final int UNICODE_WALL = 0x1F926;
+    public static final String EMOJI_JA = "<img src=\"images/smiles/ja.gif\" alt=\"ja.gif\" border=\"0\">";
+    public static final int UNICODE_JA = 0x1F642;
+    public static final String EMOJI_APPLAUS = "<img src=\"images/smiles/applaus.gif\" alt=\"applaus.gif\" border=\"0\">";
+    public static final int UNICODE_APPLAUS = 0x1F917;
 
     PostListResponseBodyConverter() {
         // package-private constructor
@@ -244,16 +274,31 @@ public class PostListResponseBodyConverter extends HtmlConverter<PostList> {
     }
 
     private String replaceEmojiImagesWithUnicode(String message) {
-        message = message.replace(EMOJI_WINK, getEmijoByUnicode(UNICODE_WINK));
-        message = message.replace(EMOJI_DRINK, getEmijoByUnicode(UNICODE_DRINK));
-        message = message.replace(EMOJI_ROFL, getEmijoByUnicode(UNICODE_ROFL));
-        message = message.replace(EMOJI_SCHUETTEL, getEmijoByUnicode(UNICODE_SCHUETTEL));
-        message = message.replace(EMOJI_FLEHAN, getEmijoByUnicode(UNICODE_FLEHAN));
-        message = message.replace(EMOJI_LOVE, getEmijoByUnicode(UNICODE_LOVE));
-        message = message.replace(EMOJI_GRIN, getEmijoByUnicode(UNICODE_GRIN));
-        message = message.replace(EMOJI_SMILE, getEmijoByUnicode(UNICODE_SMILE));
-        message = message.replace(EMOJI_EEK, getEmijoByUnicode(UNICODE_EEK));
-        message = message.replace(EMOJI_COOL, getEmijoByUnicode(UNICODE_COOL));
+        message = message.replaceAll(EMOJI_WINK, getEmijoByUnicode(UNICODE_WINK));
+        message = message.replaceAll(EMOJI_DRINK, getEmijoByUnicode(UNICODE_DRINK));
+        message = message.replaceAll(EMOJI_ROFL, getEmijoByUnicode(UNICODE_ROFL));
+        message = message.replaceAll(EMOJI_SCHUETTEL, getEmijoByUnicode(UNICODE_SCHUETTEL));
+        message = message.replaceAll(EMOJI_FLEHAN, getEmijoByUnicode(UNICODE_FLEHAN));
+        message = message.replaceAll(EMOJI_LOVE, getEmijoByUnicode(UNICODE_LOVE));
+        message = message.replaceAll(EMOJI_GRIN, getEmijoByUnicode(UNICODE_GRIN));
+        message = message.replaceAll(EMOJI_SMILE, getEmijoByUnicode(UNICODE_SMILE));
+        message = message.replaceAll(EMOJI_EEK, getEmijoByUnicode(UNICODE_EEK));
+        message = message.replaceAll(EMOJI_COOL, getEmijoByUnicode(UNICODE_COOL));
+        message = message.replaceAll(EMOJI_TONGUE, getEmijoByUnicode(UNICODE_TONGUE));
+        message = message.replaceAll(EMOJI_HAPPY, getEmijoByUnicode(UNICODE_HAPPY));
+        message = message.replaceAll(EMOJI_NIXWEISS, getEmijoByUnicode(UNICODE_NIXWEISS));
+        message = message.replaceAll(EMOJI_DAGEGEN, getEmijoByUnicode(UNICODE_DAGEGEN));
+        message = message.replaceAll(EMOJI_VIELPOSTEN, getEmijoByUnicode(UNICODE_VIELPOSTEN));
+        message = message.replaceAll(EMOJI_SMOKE, getEmijoByUnicode(UNICODE_SMOKE));
+        message = message.replaceAll(EMOJI_NAJA, getEmijoByUnicode(UNICODE_NAJA));
+        message = message.replaceAll(EMOJI_ROLLEYES, getEmijoByUnicode(UNICODE_ROLLEYES));
+        message = message.replaceAll(EMOJI_RAZZ, getEmijoByUnicode(UNICODE_RAZZ));
+        message = message.replaceAll(EMOJI_LACH, getEmijoByUnicode(UNICODE_LACH));
+        message = message.replaceAll(EMOJI_KOTZ, getEmijoByUnicode(UNICODE_KOTZ));
+        message = message.replaceAll(EMOJI_LACHLAUT, getEmijoByUnicode(UNICODE_LACHLAUT));
+        message = message.replaceAll(EMOJI_WALL, getEmijoByUnicode(UNICODE_WALL));
+        message = message.replaceAll(EMOJI_JA, getEmijoByUnicode(UNICODE_JA));
+        message = message.replaceAll(EMOJI_APPLAUS, getEmijoByUnicode(UNICODE_APPLAUS));
         return message;
     }
 

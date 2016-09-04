6c866d625cb40a8369f5f7a466e42bed85bf13b4
Oliver Degener <o.degener@monday-consulting.com>
2016-09-05 01:24:59 +0200

    prepare active topics call

--- app/src/main/java/org/ollide/stpauliforum/api/TopicService.java
+++ app/src/main/java/org/ollide/stpauliforum/api/TopicService.java
@@ -1,6 +1,7 @@
 package org.ollide.stpauliforum.api;
 
 import org.ollide.stpauliforum.model.html.PostList;
+import org.ollide.stpauliforum.model.html.TopicList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -17,7 +18,7 @@ public interface TopicService {
      * (n - 1) * 15 for offset.
      *
      * @param topicId the topic to find posts for
-     * @param offset the paging offset (15 per page)
+     * @param offset  the paging offset (15 per page)
      * @return the requested PostList
      */
     @GET("viewtopic.php?postdays=0&postorder=asc")
@@ -26,5 +27,8 @@ public interface TopicService {
 
     @GET("viewtopic.php")
     Call<PostList> getPostsByPostId(@Query("p") int topicId);
+
+    @GET("recent.php?selorder=laday")
+    Call<TopicList> getActiveTopicsInLastDays(@Query("nodays") int days);
 
 }
---
+++ app/src/main/java/org/ollide/stpauliforum/model/html/TopicList.java
@@ -0,0 +1,18 @@
+package org.ollide.stpauliforum.model.html;
+
+import org.ollide.stpauliforum.model.Topic;
+
+import java.util.List;
+
+public class TopicList {
+
+    private List<Topic> topics;
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
+}
